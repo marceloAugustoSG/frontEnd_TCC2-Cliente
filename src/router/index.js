76e25ef170099ec9ed52375ba4161e6cb76b22ca
MarceloASG <marcelo_augusto.soares@hotmail.com>
2024-03-08 20:01:45 -0300

    adicionando arquivo_redirects

--- src/router/index.js
+++ src/router/index.js
@@ -1,89 +1,76 @@
 import { createRouter, createWebHistory } from "vue-router";
-const routes =
-  [
-    {
-      path: "/",
-      redirect: "login",
+const routes = [
+  {
+    path: "/",
+    redirect: "login",
+  },
+  {
+    path: "/login",
+    name: "login",
+    beforeEnter: (to, from, next) => {
+      const autenticado = localStorage.getItem("token");
+      if (autenticado) {
+        // Se já estiver autenticado, redirecione para o dashboard
+        next("/dashboard");
+      } else {
+        // Caso contrário, continue com a navegação normal
+        next();
+      }
     },
-    {
-      path: "/login",
-      name: "login",
-      beforeEnter: (to, from, next) => {
-        const autenticado = localStorage.getItem('token');
-        if (autenticado) {
-          // Se já estiver autenticado, redirecione para o dashboard
-          next('/dashboard');
-        } else {
-          // Caso contrário, continue com a navegação normal
-          next();
-        }
 
+    component: () => import("@/pages/login/Login"),
+  },
+  {
+    path: "/criarConta",
+    component: () => import("@/pages/criarConta.vue"),
+  },
+  {
+    path: "/logout",
+    component: () => import("@/pages/login/Logout"),
+  },
+  {
+    name: "dashboard",
+    path: "/dashBoard",
+    beforeEnter: (to, from, next) => {
+      const autenticado = localStorage.getItem("token");
+      if (autenticado) {
+        next();
+      } else {
+        next("login");
+      }
+    },
+
+    component: () => import("@/pages/dashboard/Dashboard.vue"),
+
+    children: [
+      {
+        path: "/inicio",
+        component: () => import("@/pages/dashboard/Inicio.vue"),
       },
 
-      component: () => import("@/pages/login/Login"),
-    },
-    {
-      path: "/criarConta",
-      component: () => import("@/pages/criarConta.vue"),
-    },
-    {
-      path: "/logout",
-      component: () => import("@/pages/login/Logout"),
-    },
-    {
-      name: "dashboard",
-      path: "/dashBoard",
-      beforeEnter: (to, from, next) => {
-        const autenticado = localStorage.getItem('token')
-        if (autenticado) {
-          next()
-        } else {
-          next('login')
-        }
+      {
+        path: "/consultas",
+        component: () => import("@/pages/dashboard/Consultas.vue"),
+      },
+      {
+        path: "/agendar",
+        component: () => import("@/pages/dashboard/SolicitarConsulta.vue"),
       },
 
-      component: () => import("@/pages/dashboard/Dashboard.vue"),
-
-      children: [
-        {
-          path: "/inicio",
-          component: () => import("@/pages/dashboard/Inicio.vue"),
-        },
-
-        // {
-        //   path: "/teste",
-        //   component: () => import("@/components/DashBoard/FormStepper.vue"),
-        // },
-
-        // {
-        //   path: "/teste10",
-        //   component: () => import("@/components/teste10.vue"),
-        // },
-
-        {
-          path: "/consultas",
-          component: () => import("@/pages/dashboard/Consultas.vue"),
-        },
-        {
-          path: "/agendar",
-          component: () => import("@/pages/dashboard/SolicitarConsulta.vue"),
-        },
-
-        {
-          path: "/configuracoes",
-          component: () => import("@/components/DashBoard/Configuracoes.vue"),
-        },
-        {
-          path: "/perfil",
-          component: () => import("@/components/DashBoard/Perfil.vue"),
-        }
-      ]
-    }]
+      {
+        path: "/configuracoes",
+        component: () => import("@/components/DashBoard/Configuracoes.vue"),
+      },
+      {
+        path: "/perfil",
+        component: () => import("@/components/DashBoard/Perfil.vue"),
+      },
+    ],
+  },
+];
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
-
-
 
 export default router;
